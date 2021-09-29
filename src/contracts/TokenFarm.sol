c65293a0e980cab33448a0a796bfef7956f8d0d6
kevinlcaiug <kevin.l.cai.ug@dartmouth.edu>
2021-09-29 01:04:48 -0400

    Updated TokenFarm sc

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,19 +5,24 @@ import "./DaiToken.sol";
 
 contract TokenFarm {
     string public name = "Dapp Token Farm";
+    address owner;
     DappToken public dappToken;
     DaiToken public daiToken;
 
     address[] public stakers;
     mapping(address => uint) public stakingBalance;
     mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
 
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken =  _daiToken;
+        owner = msg.sender;
     }
 
     function stakeTokens(uint _amount) public {
+        // require an amount greater than 0
+        require(_amount > 0, "amount cannot be 0");
         // Transfer DAI tokens to TokenFarm for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
         // update staking balance
@@ -30,5 +35,19 @@ contract TokenFarm {
         // Update staking status
         isStaking[msg.sender] = true;
         hasStaked[msg.sender] = true;
+    }
+
+    function issueToken() public {
+        // Only the owner can call this function
+        require(msg.sender == owner, "caller must be the owner");
+        // Issue tokens to stakers
+        for (uint i=0; i<stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if(balance > 0) {
+                dappToken.transfer(recipient, balance);
+            }
+            
+        }
     }
 }
